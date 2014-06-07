7c3dc84f2bc8d573ef8887aada76e1bc95746c84
Christoph Neuroth <christoph.neuroth@gmail.com>
2014-06-07 13:51:18 +0200

    add angular routing

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,6 +1,16 @@
 angular
-    .module('bookshelfApp', [])
+    .module('bookshelfApp', ['ngRoute'])
     .value('version', 'dev')
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/shelve/:userId', {
+                controller: 'ShelveController',
+                templateUrl: 'shelve.html'
+            })
+            .otherwise({
+                redirectTo: '/shelve/foo'
+            });
+    }])
     .controller('ShelveController', function ($http, $scope) {
         $scope.books = {
             available: [],
