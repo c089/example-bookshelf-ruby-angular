6b7a55c3741b40ff380b39deda65ca27da1f8ff3
Christoph Neuroth <christoph.neuroth@gmail.com>
2014-06-08 11:17:36 +0200

    change style of registering components

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,48 +1,49 @@
-angular
-    .module('bookshelfApp', ['ngRoute'])
-    .value('version', 'dev')
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/shelve/:userId', {
-                controller: 'ShelveController',
-                templateUrl: 'shelve.html'
-            })
-            .otherwise({
-                redirectTo: '/shelve/foo'
-            });
-    }])
-    .controller('ShelveController', function ($http, $q, $routeParams, $scope) {
-        var shelfPath = function () {
-                return '/api/shelves/' + $routeParams.userId;
-            },
-            sendShelfToServer = function () {
-                var booksOnShelf = _($scope.books).where({isOnShelf: true});
-                $http.put(shelfPath(), _.pluck(booksOnShelf, 'id'));
-            };
+var mod = angular.module('bookshelfApp', ['ngRoute']);
 
-        $scope.addToShelf = function (book) {
-            book.isOnShelf = true;
-            sendShelfToServer();
-        }
+mod.value('version', 'dev');
+mod.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/shelve/:userId', {
+            controller: 'ShelveController',
+            templateUrl: 'shelve.html'
+        })
+        .otherwise({
+            redirectTo: '/shelve/foo'
+        });
+}]);
 
-        $scope.removeFromShelf = function (book) {
-            book.isOnShelf = false;
-            sendShelfToServer();
-        }
+mod.controller('ShelveController', function ($http, $q, $routeParams, $scope) {
+    var shelfPath = function () {
+            return '/api/shelves/' + $routeParams.userId;
+        },
+        sendShelfToServer = function () {
+            var booksOnShelf = _($scope.books).where({isOnShelf: true});
+            $http.put(shelfPath(), _.pluck(booksOnShelf, 'id'));
+        };
 
-        $q.all([
-            $http.get('/api/books'),
-            $http.get(shelfPath())
-        ]).then(function (results) {
-            var allBooks = results[0].data;
-            var booksOnShelve = results[1].data;
+    $scope.addToShelf = function (book) {
+        book.isOnShelf = true;
+        sendShelfToServer();
+    }
 
-            $scope.books = _.map(allBooks, function (book) {
-                var idsOfBooksOnShelve = _.pluck(booksOnShelve, 'id');
-                return _.extend({}, book, {
-                    isOnShelf: _(idsOfBooksOnShelve).contains(book.id)
-                });
+    $scope.removeFromShelf = function (book) {
+        book.isOnShelf = false;
+        sendShelfToServer();
+    }
+
+    $q.all([
+        $http.get('/api/books'),
+        $http.get(shelfPath())
+    ]).then(function (results) {
+        var allBooks = results[0].data;
+        var booksOnShelve = results[1].data;
+
+        $scope.books = _.map(allBooks, function (book) {
+            var idsOfBooksOnShelve = _.pluck(booksOnShelve, 'id');
+            return _.extend({}, book, {
+                isOnShelf: _(idsOfBooksOnShelve).contains(book.id)
             });
         });
-
     });
+
+});
