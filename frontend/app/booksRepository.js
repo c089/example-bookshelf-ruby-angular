fb4052dd242b042a5f4cf3582af8f66d19074a0b
Christoph Neuroth <christoph.neuroth@gmail.com>
2014-06-10 11:41:16 +0200

    extract method

--- frontend/app/booksRepository.js
+++ frontend/app/booksRepository.js
@@ -2,22 +2,20 @@ angular.module('bookshelfApp.booksRepository', [])
     .factory('BooksRepository', ['$http', function ($http) {
         var shelfPath = function (userId) {
             return '/api/shelves/' + userId;
+        }, extractData = function (result) {
+            return result.data;
         };
 
         return {
             retrieveBooks: function () {
-                return $http.get('/api/books').then(function (result) {
-                    return result.data;
-                });
+                return $http.get('/api/books').then(extractData);
             },
             createBook: function (bookData) {
                 return $http.post('/api/books', bookData)
-                    .then(function (result) { return result.data; });
+                    .then(extractData);
             },
             retrieveShelf: function(userId) {
-                return $http.get(shelfPath(userId)).then(function (result) {
-                    return result.data;
-                });
+                return $http.get(shelfPath(userId)).then(extractData);
             },
             updateShelf: function(userId, books) {
                 return $http.put(shelfPath(userId), _.pluck(books, 'id'));
